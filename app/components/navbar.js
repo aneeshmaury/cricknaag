ad3d501e4ff361884128779369a4ac05248624c9
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-03-06 12:19:41 +0530

    hhh

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,14 +3,14 @@ import { FaShoppingBag } from "react-icons/fa";
 export default function Navbar() {
   return (
    
-<div className="flex justify-between p-6 border-b-2">
+<div className="flex justify-between py-2 px-9 border-b-2">
 <div className="logo">
     <img className="w-[50px] bg-black rounded-full" src="/logo.png.png"/>
 </div>
 
-<div className="right-side rounded-xl bg-yellow-300 flex items-center justify-center gap-3  w-[120px]">
-    <div className="text-3xl"> <FaShoppingBag/></div>
-    <p className="font-bold">Shop</p>
+<div className="right-side rounded-sm bg-black text-white flex items-center justify-center gap-3 h-[50px]  w-[100px]">
+    <div className="text-xl"> <FaShoppingBag/></div>
+    <p className="font-semibold">Shop</p>
 </div>
 
 
